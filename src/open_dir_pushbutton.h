8cf5a4631c287346c75d07049b27a23e576f72f7
Arjentix <arjentix@gmail.com>
2020-02-01 00:48:36 +0300

    Added AGPL 3.0 license

--- src/open_dir_pushbutton.h
+++ src/open_dir_pushbutton.h
@@ -1,3 +1,22 @@
+/**
+ * Akiner - crossplatform Qt-based app which helps you to delete, move or copy akin files.
+ * Copyright (C) 2020  Polyakov Daniil
+ * Email address: arjentix@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef OPENDIRPUSHBUTTON_H
 #define OPENDIRPUSHBUTTON_H
 
